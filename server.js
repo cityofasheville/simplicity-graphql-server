00eb93abcaa249a71b43fc2394f35aeb5b480b0d
ejaxon <eric@deepweave.com>
2016-10-11 13:22:59 -0400

    Added token verification on the server

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const Pool = pg.Pool;
 import { subscriptionManager } from './data/subscriptions';
 import schema from './data/schema';
 
-// DATABASE_URL: :@:5432/
+// Import Firebase - for now (8/25/16), the use of require and import of individual
+// submodules is needed to avoid problems with webpack (import seems to require
+// beta version of webpack 2).
+const firebase = require('firebase');
+
+firebase.initializeApp({
+  serviceAccount: "./SimpliCityII-284f9d0ebb83.json",
+  databaseURL: "https://simplicityii-878be.firebaseio.com"
+});
 
 var dbConfig = {
   host: 'ec2-54-235-65-139.compute-1.amazonaws.com',
@@ -27,14 +35,67 @@ const WS_PORT = 8090;
 
 const graphQLServer = express().use('*', cors());
 
+// Decoded token: {
+//   "iss":"https://securetoken.google.com/simplicityii-878be",
+//   "name":"Eric Jackson",
+//   "picture":"https://lh3.googleusercontent.com/-YfuE6u4uQpE/AAAAAAAAAAI/AAAAAAAAAI8/aMN1TtvIV_I/s96-c/photo.jpg",
+//   "aud":"simplicityii-878be",
+//   "auth_time":1475009898,
+//   "user_id":"B9ewtFNqm0a9yOu2ljdHSEwkRS92",
+//   "sub":"B9ewtFNqm0a9yOu2ljdHSEwkRS92",
+//   "iat":1476205771,
+//   "exp":1476209371,
+//   "email":"eric@deepweave.com",
+//   "email_verified":true,
+//   "firebase":{
+//     "identities":{
+//       "google.com":["113759490376150867470"],
+//       "email":["eric@deepweave.com"]
+//     },
+//     "sign_in_provider":"google.com"
+//   },
+//   "uid":"B9ewtFNqm0a9yOu2ljdHSEwkRS92"
+// }
 graphQLServer.use('/graphql', bodyParser.json(), apolloExpress( (req, res) => {
-  return {
-    schema,
-    context: {
-      pool,
-      token: req.headers.authorization,
-    },
-  };
+  if (!req.headers.authorization) {
+    return {
+      schema,
+      context: {
+        pool,
+        loggedin: false,
+        token: null,
+        uid: null,
+        name: null,
+        email: null,
+      },
+    };
+  }
+  return firebase.auth().verifyIdToken(req.headers.authorization).then (function (decodedToken) {
+    return {
+      schema,
+      context: {
+        pool,
+        loggedin: true,
+        token: req.headers.authorization,
+        uid: decodedToken.uid,
+        name: decodedToken.name,
+        email: decodedToken.email,
+      },
+    };
+  }).catch (function(error) {
+    console.log("Error decoding firebase token: " + JSON.stringify(error));
+    return {
+      schema,
+      context: {
+        pool,
+        loggedin: false,
+        token: null,
+        uid: null,
+        name: null,
+        email: null,
+      },
+    };
+  });
 }));
 
 graphQLServer.use('/graphiql', graphiqlExpress({
