fff7a1f6f467072127692341bb006069e166fe70
Eric Jackson <eric@deepweave.com>
2017-01-30 14:06:04 -0500

    Added NGINX config to deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
-# Add the services
+# Install the services
 cp ./simplicitygql1.service /etc/systemd/system/
 cp ./simplicitygql1.service /etc/systemd/system/
 
@@ -8,4 +8,7 @@ systemctl start simplicitygql2
 systemctl enable simplicitygql1
 systemctl enable simplicitygql2
 
+# Install the NGINX configuration file
+cp ./simplicitygql.conf /etc/nginx/conf.d/
+sudo systemctl restart nginx
 
