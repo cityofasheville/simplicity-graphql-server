3ad2a364bf163817c5452e2f31e00083b3c534e4
Eric Jackson <ejackson@ashevillenc.gov>
2017-09-08 15:11:11 -0400

    Optimized the search

--- data/simplicity/search/resolvers.js
+++ data/simplicity/search/resolvers.js
@@ -31,24 +31,27 @@ function searchCivicAddressId(searchString, context) {
 }
 
 function searchAddress(searchString, searchContext, context) {
-  // const geolocatorUrl = 'http://192.168.0.125:6080/arcgis/rest/services/Geolocators/BC_address_unit/GeocodeServer/findAddressCandidates'
+  const maxCandidates = 500;
+  const minCandidateScore = 50;
   const geolocatorUrl = 'http://arcgis.ashevillenc.gov/arcgis/rest/services/Geolocators/BC_address_unit/GeocodeServer/findAddressCandidates'
   + '?Street=&City=&ZIP='
   + `&Single+Line+Input=${encodeURIComponent(searchString)}&category=`
   + '&outFields=House%2C+PreDir%2C+StreetName%2C+SufType%2C+SubAddrUnit%2C+City%2C+ZIP'
   + '&maxLocations=&outSR=&searchExtent='
   + '&location=&distance=&magicKey=&f=pjson';
-  console.log('Making the call');
-  console.log(geolocatorUrl);
+
   return axios.get(geolocatorUrl, { timeout: 5000 })
   .then(response => {
-    console.log(`Got a total of ${response.data.candidates.length} responses`);
-    // console.log(JSON.stringify(response.data.candidates));
     const candidates = response.data.candidates.filter(c => {
-      return (c.score > 50);
+      return (c.score >= minCandidateScore);
     });
     if (candidates.length === 0) {
-      return Promise.resolve([]);
+      return Promise.resolve(
+        {
+          type: 'searchContext',
+          results: [],
+        }
+      );
     }
 
     const locNumber = [];
@@ -59,53 +62,50 @@ function searchAddress(searchString, searchContext, context) {
     const locZipcode = [];
     const locCity = [];
     candidates.forEach((c, i) => {
-      locNumber.push(c.attributes.House);
-      locName.push(c.attributes.StreetName);
-      locType.push(c.attributes.SufType);
-      locPrefix.push(c.attributes.PreDir);
-      locUnit.push(c.attributes.SubAddrUnit);
-      locZipcode.push(c.attributes.ZIP);
-      locCity.push(c.attributes.City);
+      if (i < maxCandidates) {
+        locNumber.push(c.attributes.House);
+        locName.push(c.attributes.StreetName);
+        locType.push(c.attributes.SufType);
+        locPrefix.push(c.attributes.PreDir);
+        locUnit.push(c.attributes.SubAddrUnit);
+        locZipcode.push(c.attributes.ZIP);
+        locCity.push(c.attributes.City);
+      }
     });
 
     const fquery = 'SELECT civicaddress_id, address_full, address_city, address_zipcode, '
     + 'address_number, address_unit, address_street_prefix, address_street_name '
-    + 'from amd.get_all_foo($1, $2, $3, $4, $5, $6, $7)';
+    + 'from amd.get_search_addresses($1, $2, $3, $4, $5, $6, $7)';
     const args = [locNumber, locName, locType, locPrefix, locUnit, locZipcode, locCity];
 
     const idMap = {};
     return context.pool.query(fquery, args)
     .then(result => {
-      console.log(`RESULT: ${JSON.stringify(result.rows)}`);
-      return {
-        items: result.rows.map(row => {
-          return {
-            score: 0,
-            type: 'address',
-            civic_address_id: row.civicaddress_id,
-            address: row.address_full,
-            street_name: row.address_street_name,
-            street_prefix: row.address_street_prefix,
-            street_number: row.address_number,
-            unit: row.address_unit,
-            city: row.address_city,
-            zipcode: row.address_zipcode,
-          };
-        })
-        // .filter(row => {
-        //   if (idMap.hasOwnProperty(row.civic_address_id)) return false;
-        //   idMap[row.civic_address_id] = true;
-        //   return true;
-        // })
-        ,
-      };
+      return result.rows.map(row => {
+        return {
+          score: 0,
+          type: 'address',
+          civic_address_id: row.civicaddress_id,
+          address: row.address_full,
+          street_name: row.address_street_name,
+          street_prefix: row.address_street_prefix,
+          street_number: row.address_number,
+          unit: row.address_unit,
+          city: row.address_city,
+          zipcode: row.address_zipcode,
+        };
+      })
+      .filter(row => {
+        if (idMap.hasOwnProperty(row.civic_address_id)) return false;
+        idMap[row.civic_address_id] = true;
+        return true;
+      })
+      ;
     })
     .then(clist => {
       const result = {
         type: 'searchContext',
-        results: clist.reduce((prev, curr) => {
-          return prev.concat(curr.items);
-        }, []),
+        results: clist,
       };
       return Promise.resolve(result);
     });
