30680003888c5fca48a097ba39868b8364374352
Eric Jackson <ejackson@ashevillenc.gov>
2017-11-22 12:30:59 -0500

    Added properties_by_street endpoint

--- data/simplicity/simplicity_resolvers.js
+++ data/simplicity/simplicity_resolvers.js
@@ -204,15 +204,55 @@ const resolvers = {
         throw new Error(error);
       });
     },
-    // properties_by_street(obj, args, context) {
-    //   const pool = context.pool;
-    //   const radius = Number(args.radius); // State plane units are feet
-    //   const ids = args.centerline_ids;
-    //   if (ids.length <= 0) return [];
-    //   const idList = ids.map(id => {
-    //     return `'${id}'`;
-    //   }).join(',');
-    // },
+    properties_by_street(obj, args, context) {
+      const pool = context.pool;
+      const radius = (args.radius) ? Number(args.radius) : 100; // State plane units are feet
+      const ids = args.centerline_ids;
+      if (ids.length <= 0) return [];
+      const query = 'SELECT * FROM amd.get_properties_along_streets($1, $2)';
+      const fargs = [
+        ids,
+        radius,
+      ];
+      return pool.query(query, fargs)
+      .then(result => {
+        if (result.rows.length === 0) return [];
+        return result.rows.map(itm => {
+          const taxExempt = (itm.exempt !== '');
+          return {
+            civic_address_id: itm.civicaddress_id,
+            pinnum: itm.pin,
+            pinnumext: itm.pinext,
+            address: itm.address,
+            city: itm.cityname,
+            zipcode: itm.zipcode,
+            tax_exempt: taxExempt,
+            neighborhood: itm.neighborhoodcode,
+            appraisal_area: itm.appraisalarea,
+            acreage: itm.acreage,
+            zoning: itm.zoning,
+            deed_link: itm.deedurl,
+            property_card_link: itm.propcard,
+            plat_link: itm.platurl,
+            latitude: itm.lattitude,
+            longitude: itm.longitude,
+            building_value: itm.buildingvalue,
+            land_value: itm.landvalue,
+            appraised_value: itm.appraisedvalue,
+            tax_value: itm.taxvalue,
+            market_value: itm.totalmarketvalue,
+            owner: itm.owner,
+            owner_address: itm.owner_address,
+          };
+        });
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(`Got an error in properties_by_street: ${JSON.stringify(err)}`);
+          throw new Error(err);
+        }
+      });
+    },
     addresses(obj, args, context) {
       const pool = context.pool;
       const ids = args.civicaddress_ids;
