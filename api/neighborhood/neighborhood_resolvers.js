d7ac3c24be685a1982d95686a4e6b4640968ea6a
Patrick Conant <conantp@prcappss-mbp.lan>
2018-08-20 10:45:46 -0400

    Cleaned up neighborhood resolver

--- api/neighborhood/neighborhood_resolvers.js
+++ api/neighborhood/neighborhood_resolvers.js
@@ -5,20 +5,17 @@ const resolvers = {
     neighborhoods(obj, args, context) {
       const logger = context.logger;
 
-      var argData = [];
+      var query_args = [];
       var query = 'SELECT name, nbhd_id, abbreviation, narrative, '
       + 'st_astext(st_transform(shape, 4326)) AS polygon '
       + 'FROM amd.coa_asheville_neighborhoods ';
 
-      if (typeof args.nbrhd_ids === typeof undefined || args.nbrhd_ids.length <= 0) {
-
-      }
-      else{
+      if (typeof args.nbrhd_ids !== typeof undefined && args.nbrhd_ids.length > 0) {
         query += ' WHERE nbhd_id = ANY ($1)';
-        argData = [args.nbrhd_ids];
+        query_args.push(args.nbrhd_ids);
       }
      
-      return context.pool.query(query, argData)
+      return context.pool.query(query, query_args)
       .then(result => {
         if (result.rows.length === 0) return [];
         return result.rows.map(itm => {
