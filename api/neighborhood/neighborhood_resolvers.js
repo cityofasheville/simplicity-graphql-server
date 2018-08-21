e5cdd719933ccbee671403613b858f297fc9a5ad
Eric Jackson <ejackson@ashevillenc.gov>
2018-08-21 13:35:47 -0400

    Revert "AVL-RN: Modify neighborhoods endpoint so no args returns all neighborhoods. Also add dockerfile."
    This reverts commit 839fd9336e4a6b93d9b02b0a88761d5c63392de2.

--- api/neighborhood/neighborhood_resolvers.js
+++ api/neighborhood/neighborhood_resolvers.js
@@ -4,18 +4,11 @@ const resolvers = {
   Query: {
     neighborhoods(obj, args, context) {
       const logger = context.logger;
-
-      var query_args = [];
-      var query = 'SELECT name, nbhd_id, abbreviation, narrative, '
+      if (args.nbrhd_ids.length <= 0) return [];
+      const query = 'SELECT name, nbhd_id, abbreviation, narrative, '
       + 'st_astext(st_transform(shape, 4326)) AS polygon '
-      + 'FROM amd.coa_asheville_neighborhoods ';
-
-      if (typeof args.nbrhd_ids !== typeof undefined && args.nbrhd_ids.length > 0) {
-        query += ' WHERE nbhd_id = ANY ($1)';
-        query_args.push(args.nbrhd_ids);
-      }
-     
-      return context.pool.query(query, query_args)
+      + 'FROM amd.coa_asheville_neighborhoods WHERE nbhd_id = ANY ($1)';
+      return context.pool.query(query, [args.nbrhd_ids])
       .then(result => {
         if (result.rows.length === 0) return [];
         return result.rows.map(itm => {
