e175a6398d7902ce1314a94774ac494037ce0d12
Jon Wilson <jontwilson@gmail.com>
2023-07-21 12:39:07 -0400

    geocoder-new vers sqls

--- database/functions/get_search_streets.sql
+++ database/functions/get_search_streets.sql
@@ -1,38 +1,22 @@
--- FUNCTION: simplicity.get_search_streets(character varying[], integer[])
 
--- DROP FUNCTION simplicity.get_search_streets(character varying[], integer[]);
+-- DROP FUNCTION simplicity.get_search_streets(character varying[], character varying[], character varying[]);
 
-CREATE OR REPLACE FUNCTION simplicity.get_search_streets(
-	lstreetname character varying[],
-	lzipcode integer[])
-    RETURNS SETOF simplicity.v_simplicity_streets 
-    LANGUAGE 'plpgsql'
-
-    COST 100
-    VOLATILE 
-    ROWS 1000
-AS $BODY$
-
-DECLARE
-    r simplicity.v_simplicity_streets%rowtype;
-BEGIN
-	for i in 1..array_length(lstreetname,1) loop
-    	FOR r IN (
-                select centerline_id, full_street_name, lzip as left_zipcode, rzip as right_zipcode,
-						left_from_address, right_from_address,
-						left_to_address, right_to_address,																					
-            			st_astext(st_transform(shape, 4326)) AS line
-                from internal.bc_street 
-                where street_name = lstreetname[i]
-                and   (lzip = lzipcode[i] OR rzip = lzipcode[i])
-            )
-		LOOP
-			RETURN NEXT r; -- return current row of SELECT
-        END LOOP;
-    END LOOP;
-    RETURN;
+CREATE OR REPLACE FUNCTION simplicity.get_search_streets(lstreetname character varying[], lzipcode character varying[], lprefix character varying[])
+ RETURNS SETOF simplicity.v_simplicity_streets
+ LANGUAGE plpgsql
+AS $function$
+begin
+	return query select centerline_id, full_street_name, lzip as left_zipcode, rzip as right_zipcode,
+			left_from_address, right_from_address,
+			left_to_address, right_to_address,																					
+			st_astext(st_transform(shape, 4326)) AS line
+	from internal.bc_street str
+	inner join unnest(lstreetname,lzipcode,lprefix) as params(lstreetname,lzipcode,lprefix)
+	on str.street_name = params.lstreetname
+	and   (str.lzip::text = params.lzipcode OR params.lzipcode = '')
+	and   (str.street_prefix = params.lprefix OR params.lprefix = '');
 END
-
-$BODY$;
+$function$
+;
 
 
