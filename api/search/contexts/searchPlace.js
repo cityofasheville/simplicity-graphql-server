23ac23394cb8485e55797c40fff38fb1350a188e
Jon Wilson <jontwilson@gmail.com>
2024-01-03 14:34:05 -0500

    npmaudit

--- api/search/contexts/searchPlace.js
+++ api/search/contexts/searchPlace.js
@@ -4,7 +4,8 @@ const { get } = axiospkg;
 function searchPlace(searchString, context) {
   const logger = context.logger;
   const keyword = searchString.replace(/\s/g, '%20');
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBy-YYl13oQZBLm-v1udn5Thk1JSPuOVR0&location=35.5951,-82.5515&radius=50000&keyword=${keyword}`;
+  const googleapikey = '';
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${googleapikey}&location=35.5951,-82.5515&radius=50000&keyword=${keyword}`;
   return get(url, { timeout: 5000 })
   .then(response => {
     return response.data.results.map(place => {
