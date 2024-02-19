cc3e99dfb7353faa1a8acdde9377ea9808a785f7
Jon Wilson <jontwilson@gmail.com>
2024-02-19 13:01:28 -0500

    refactor out set_up_server code

--- src/local.server.js
+++ src/local.server.js
@@ -1,64 +1,14 @@
-import typeDefs from './schema.js';
-import resolvers from './resolvers.js';
 
-import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone'; 
-import "dotenv/config.js";
-
-import pgpkg from 'pg';
-const { Pool, defaults } = pgpkg;
-
-import mssqlpkg from 'mssql';
-const { connect } = mssqlpkg;
-
-defaults.poolSize = 1;
-
-// PLAYGROUND
-let debug = true;
-
-let introspection = false;
-let playground = false;
-if (debug) {
-  introspection = true;
-  playground = true;
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection,
-  playground,
-});
-
-const dbConfig = {
-  host: process.env.dbhost,
-  user: process.env.dbuser,
-  password: process.env.dbpassword,
-  database: process.env.database,
-  port: 5432,
-  ssl: false,
-};
-const dbConfig_accela = {
-  user: process.env.dbuser_accela,
-  password: process.env.dbpassword_accela,
-  server: process.env.dbhost_accela,
-  domain: process.env.dbdomain_accela,
-  database: process.env.database_accela,
-  options: { enableArithAbort: true,  encrypt: false },
-  connectionTimeout: 30000,
-  requestTimeout: 680000,
-  trustServerCertificate: true,  // Acella has self-signed certs?
-};
+import { 
+  server,
+  pool,
+  pool_accela
+} from "./set_up_server.js";
 
 (async () => {
   try {
-    console.info('Connect to database');
-    const pool = new Pool(dbConfig);
-    const pool_accela = await connect(dbConfig_accela);
-
-    console.info('Database connection initialized');
-
-    const GRAPHQL_PORT = process.env.PORT || 8080;
+    const GRAPHQL_PORT = 8080;
 
     const { url } = await startStandaloneServer(server, {
       context: () => {
---
+++ src/set_up_server.js
@@ -0,0 +1,60 @@
+import typeDefs from './schema.js';
+import resolvers from './resolvers.js';
+
+import { ApolloServer } from '@apollo/server';
+import "dotenv/config.js";
+
+import pgpkg from 'pg';
+const { Pool, defaults } = pgpkg;
+
+import mssqlpkg from 'mssql';
+const { connect } = mssqlpkg;
+
+defaults.poolSize = 1;
+
+// PLAYGROUND
+let debug = true;
+
+let introspection = false;
+let playground = false;
+if (debug) {
+  introspection = true;
+  playground = true;
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection,
+  playground,
+});
+
+const dbConfig = {
+  host: process.env.dbhost,
+  user: process.env.dbuser,
+  password: process.env.dbpassword,
+  database: process.env.database,
+  port: 5432,
+  ssl: false,
+};
+const dbConfig_accela = {
+  user: process.env.dbuser_accela,
+  password: process.env.dbpassword_accela,
+  server: process.env.dbhost_accela,
+  domain: process.env.dbdomain_accela,
+  database: process.env.database_accela,
+  options: { enableArithAbort: true,  encrypt: false },
+  connectionTimeout: 30000,
+  requestTimeout: 680000,
+  trustServerCertificate: true,  // Acella has self-signed certs?
+};
+
+console.info('Connect to database');
+const pool = new Pool(dbConfig);
+const pool_accela = await connect(dbConfig_accela);
+
+export { 
+  server,
+  pool,
+  pool_accela
+}
